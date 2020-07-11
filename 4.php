9ec36a6f8955b24566d7df035782366b1d0380b0
tlaalstlr <59640360+tlaalstlr@users.noreply.github.com>
2020-07-11 22:13:22 +0900

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <script>
+      name = "egoing";
+      document.write("안녕하세요."+name);
+    </script>
+    <h1>php</h1>
+    <?php
+      $name = "egoing";
+      echo "안녕하세요.".$name;
+    ?>
+  </body>
+</html>
